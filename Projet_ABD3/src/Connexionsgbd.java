5170ed5a67210bebd1afe7e62cebccac7afdc992
Omegah <Eymeric.ber@gmail.com>
2016-01-14 11:25:00 +0100

    La base

---
+++ Projet_ABD3/src/Connexionsgbd.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+import java.io.*;
+import java.sql.*;
+
+public class Connexionsgbd {
+	private static final String configurationFile = "BD.properties.txt";
+
+	public static void main(String args[]) {
+		try {
+			String jdbcDriver, dbUrl, username, password;
+			DatabaseAccessProperties dap = new DatabaseAccessProperties(
+					configurationFile);
+			jdbcDriver = dap.getJdbcDriver();
+			dbUrl = dap.getDatabaseUrl();
+			username = dap.getUsername();
+			password = dap.getPassword();
+			// Load the database driver
+			Class.forName(jdbcDriver);
+
+			Connection conn = DriverManager.getConnection(dbUrl, username,
+					password);
+			
+			//requetesbd.nbartistes(conn);
+
+			SQLWarningsException.printWarnings(conn);
+			conn.close();
+		} catch (SQLException se) {
+
+			SQLWarningsException.printExceptions(se);
+			return;
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+	}
+}
